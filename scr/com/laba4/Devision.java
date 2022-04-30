004b6d4755c886e527f810aa4e0f4ef001a538ac
MatveiSviridov <matveisvirid@gmail.com>
2022-04-30 09:21:51 +0300

    Созданы классы, подключена библиотека opencsv через maven

---
+++ scr/com/laba4/Devision.java
@@ -0,0 +1,10 @@
+package com.laba4;
+
+public class Devision {
+    public int id;
+    public String name;
+    public String sex;
+    public Devision devision;
+    public int salary;
+    public String birthDate;
+}
--- scr/com/laba4/laba4.java
+++ scr/com/laba4/laba4.java
@@ -1,4 +1,7 @@
 package com.laba4;
 
+import com.opencsv.CSVParser;
+
 public class laba4 {
+
 }
